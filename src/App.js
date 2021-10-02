9f81b2572f0670c7c7e063227f1871b92a1518aa
lakshman <nyludaya@gmail.com>
2021-10-02 22:06:31 +0530

    Fix eslint errors

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <button className="btn btn-primary">test</button>
+    </div>
+  );
 }
 
 export default App;
